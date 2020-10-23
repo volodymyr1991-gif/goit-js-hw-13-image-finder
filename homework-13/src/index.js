30017054725939a66d3d99a2f0444c683cc57472
Volodymyr Molodynia <molodunja91@gmail.com>
2020-10-23 14:34:58 +0300

    add botstrap

--- homework-13/src/index.js
+++ homework-13/src/index.js
@@ -2,6 +2,8 @@ import './styles.css';
 // import fechArticles from './js/fecch-articles'
 import updateArticlesMarcup from './js/articles'
 import newsServices from './js/news-services'
+import PNotify from 'pnotify/dist/es/PNotify.js';
+import 'pnotify/dist/PNotifyBrightTheme.css';
 // import  './templates/articles.hbs'
 import './js/articles'
 import refs from './refs'
@@ -15,19 +17,44 @@ refs.galleryContainer.innerHTML = '';
 // form.reset();
 
 newsServices.resetPage();
-// refs.galleryContainer.innerHTML = '';
+refs.galleryContainer.innerHTML = '';
 newsServices.fechArticles().then(data=>{
-    updateArticlesMarcup(data);
+    
+    if(data){
+        // refs.galleryContainer.innerHTML=''
+        setTimeout(() => {
+            // console.log(data),
+            PNotify.error({
+              text: 'ваш запит пройшов успішно',
+              delay: 2000,
+            });
+          }, 600);
+          updateArticlesMarcup(data)
+          refs.loadMoreBtn.classList.remove('is-hidden')
+          console.log( document.documentElement.offsetHeight);
+    }
+    // else if(newsServices.query.length >= 3){
+    //     updateArticlesMarcup(data);
+    //     // refs.loadMoreBtn.classList.remove('is-hidden')
+    //     console.log(data);
+    // }
+       
+    
+    console.log(newsServices.query.length);
+    
+    // refs.galleryContainer.innerHTML=''
+    // updateArticlesMarcup(data);
+    // refs.galleryContainer.innerHTML=''
     // console.log(document.documentElement.offsetHeight);
     
   
-    refs.loadMoreBtn.classList.remove('is-hidden')
+    // refs.loadMoreBtn.classList.remove('is-hidden')
 });
 });
 
 refs.loadMoreBtn.addEventListener('click', ()=>{
     newsServices.fechArticles().then(data=>{
-        console.log(data.value);
+        // console.log(data.value);
         updateArticlesMarcup(data)
         
     });
@@ -35,7 +62,7 @@ refs.loadMoreBtn.addEventListener('click', ()=>{
  console.log( document.documentElement.offsetHeight);
 
     window.scrollTo({
-       top: 500,
+       top: 1350,
             behavior: 'smooth'
         })
 })
